4076e06cc9088332dc696899ae80063a03be16f9
Viraj Naik <virajnaik8055@gmail.com>
2025-06-27 16:30:09 +0530

    Fix modules and schema syntax

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-import mongoose from "mongoose";
-let cached = global.mongoose || {conn:null, promise:null};
-export default async function connectDB (){
-     if(cached.conn) return cached.conn;
-     if(!cached.promise){
-          cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose)=>mongoose);
-     }
-     try{
-          cached.conn = await cached.promise;
-     }catch(e){
-          console.error("error occured", e);
-          cached.promise = null;
-          throw e;
-     }
-     return cached.conn;
-}
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'your-db-name',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
